024c87ab751a2f53cf272786ad19a883c90fded1
xuanranran <juanshengyuan@gmail.com>
2023-11-26 10:42:23 +0800

    Update Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -183,7 +183,7 @@ export default {
       options: {
         clientTypes: {
           Clash: "clash",
-	  Sing-box: "singbox",
+	  Singbox: "singbox",
           Surge3: "surge&ver=3",
           Surge4: "surge&ver=4",
           Quantumult: "quan",
